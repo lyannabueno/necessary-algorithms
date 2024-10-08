c1a7f444945917e1349743c042519fa696872592
ANA FLÁVIA BUENO GOMES <06006399@acad.unifeso.lan>
2024-10-08 20:56:40 -0300

    fourth project

--- ex_04.py
+++ ex_04.py
@@ -1,39 +1,87 @@
-usuarios = {
-    'ana': ['1234', 15000],
-    'julia': ['5678', 14000],
-    'luiza': ['9012', 13000],
-    'maria': ['3456', 12000]
-}
-
-print("[1] LOGIN\n[2] REGISTER\n[3] EXIT")
-
-opcao = int(input("Choose an option: "))
-
-if opcao == 1:
-       
-    login = input("Enter your username: ")
-    senha = input("Enter your password: ")   
-       
-    while login != usuarios.keys() or senha != usuarios[usuarios.keys()][0]:
+def cadastro_conta():
+    login = input("\nEnter a username: ")
     
-        login = input("Enter your username: ")
-        senha = input("Enter your password: ")
+    if login in usuarios.keys():
+        print("\nThis user is already registered!\n")
         
-        if login == usuarios.keys() and senha == usuarios[usuarios.keys()][0]:
-            print("You are logged in.")
-            
-        print("Invalid username or password.")
-            
-elif opcao == 2:
+    else:
+        senha = input("Enter a password: ")
+        usuarios[login] = {'senha': senha, 'saldo': 0}
+        print("\nUser registered successfully!\n")
+
+def login_conta():
+    login = input("\nEnter your username: ")
+    senha = input("Enter your password: ")
     
-    login = input("Enter a username: ")
+    if login in usuarios and usuarios[login]['senha'] == senha:
+        print("\nLogin successful!\n")
+    else:
+        print("\nLogin or password failed!\n")
+
+usuarios = {} # de início, o dicionário de usuários / senha está vazio
+saldo = 0
+
+print("[1] LOGIN\n[2] REGISTER\n[7] EXIT")
+
+opcao = int(input("\nChoose an option: "))
+
+while opcao != 7:
     
-    while login == usuarios.keys():
-        print("This username is already in use.")
-        login = input("Enter a username: ")
+    if opcao == 1:
+        login_conta()   
         
-    senha = input("Enter a password: ")
+        print("\n[3] DEPÓSITO\n[4] SAQUE\n[5] TRANSFERÊNCIA DE CONTA\n[6] CONSULTA DE EXTRATO")
+        
+        opcao = int(input("\nChoose an option: "))
+        
+        if opcao == 3:
+            valor = float(input("\nEnter the amount to be deposited: "))
+            saldo += valor
+            print("\nDeposit successful!\n")
+            
+        elif opcao == 4:
+            valor = float(input("\nEnter the amount to be withdrawn: "))
+            
+            if saldo >= valor:
+                saldo -= valor
+                print("\nWithdrawal successful!\n")
+            else:
+                print("\nInsufficient balance!\n")
+        
+        elif opcao == 5:
+            valor = float(input("\nEnter the amount to be transfer: "))
+            
+            if saldo >= valor:
+                user_transfer = input("\nEnter the username to transfer: ")
+                senha = input("Enter the password: ")
+                
+                if user_transfer in usuarios.keys() and usuarios[user_transfer]['senha'] == senha:
+                    usuarios[user_transfer]['saldo'] += valor
+                    saldo -= valor
+                    print("\nTransfer successful!\n")
+                    
+                elif user_transfer not in usuarios.keys():
+                    print("\nUser not found!\n")
+                    
+                else:
+                    print("\nWrong password!\n")
+
+            else:
+                print("\nInsufficient balance!\n")            
+                
+        elif opcao == 6:
+            print("\nSeu saldo é de R$ %.2f", saldo)
+            print("Histórico de suas transações:")
+            
+    elif opcao == 2:
+        cadastro_conta()
     
-    usuarios[login] = [senha, 10000]
+    elif opcao == 7:
+        print("\nExiting...")
+        break
+      
+    print(usuarios)
     
-    print("You have successfully registered.")            
+    print("\n[1] LOGIN\n[2] REGISTER\n[7] EXIT")
+
+    opcao = int(input("\nChoose an option: "))
