ae262866169c3fd637dd5c05bcbce0490a3c30f2
Lyanna Bueno <anabuenogomes@hotmail.com>
2024-10-20 20:32:00 -0300

    fourth project

--- atm.py
+++ atm.py
@@ -1,104 +1,92 @@
-def deposito():
-    valor = float(input("\nEnter the amount to be deposited: "))
-    usuarios[login]['saldo'] += valor
-    print("\nDeposit successful!\n")
-    print(usuarios)
+class ContaBancaria:
+    def __init__(self, usuario, senha):
+        self.usuario = usuario
+        self.senha = senha
+        self.saldo = 0
+        self.registro_transferencias = []
 
-def saque():
-    valor = float(input("\nEnter the amount to be withdrawn: "))
-    
-    if usuarios[login]['saldo'] >= valor:
-        usuarios[login]['saldo'] -= valor
-        print("\nWithdrawal successful!\n")
-        print(usuarios)
-    else:
-        print("\nInsufficient balance!\n")   
+    def deposito(self):
+        valor = float(input("\nEnter the amount to be deposited: "))
+        self.saldo += valor
+        print("\nDeposit successful!\n")
+        print(f"Saldo atual: {self.saldo}")
 
-def transferencia():
-    valor = float(input("\nEnter the amount to be transfer: "))
-    
-    if usuarios[login]['saldo'] >= valor:
-        user_transfer = input("\nEnter the username to transfer: ")
-        senha = input("Enter the password: ")
-        
-        if user_transfer in usuarios and usuarios[login]['senha'] == senha:
-            usuarios[login]['saldo'] -= valor
-            usuarios[user_transfer]['saldo'] += valor
-            print("\nTransfer successful!\n")
-            registro_transferencias.append((valor, user_transfer))
-            print(usuarios)
-            
-        elif user_transfer not in usuarios:
-            print("\nUser not found!\n")
-            
+    def saque(self):
+        valor = float(input("\nEnter the amount to be withdrawn: "))
+        if self.saldo >= valor:
+            self.saldo -= valor
+            print("\nWithdrawal successful!\n")
         else:
-            print("\nWrong password!\n")
-            
-    else:
-        print("\nInsufficient balance!\n")
+            print("\nInsufficient balance!\n")
+        print(f"Saldo atual: {self.saldo}")
 
-def consulta_extrato():
-    print("\nYour balance is R$ %.2f" % usuarios[login]['saldo']) # insere o valor do saldo do usuário dentro da string formatada
-    print("\nHistory of your transactions: ", registro_transferencias)
+    def transferencia(self, destinatario):
+        valor = float(input("\nEnter the amount to be transferred: "))
+        if self.saldo >= valor:
+            senha = input("\nEnter your password: ")
+            if self.senha == senha:
+                self.saldo -= valor
+                destinatario.saldo += valor
+                self.registro_transferencias.append((valor, destinatario.usuario))
+                print("\nTransfer successful!\n")
+            else:
+                print("\nWrong password!\n")
+        else:
+            print("\nInsufficient balance!\n")
 
-usuarios = {} # de início, o dicionário de usuários / senha está vazio
-registro_transferencias = []
-saldo = 0
-opcao = 0
+    def consulta_extrato(self):
+        print(f"\nYour balance is R$ {self.saldo:.2f}")
+        print("\nHistory of your transactions:", self.registro_transferencias)
 
-while opcao != 8:
-    
+usuarios = {}
+
+while True:
     print("\n[1] LOGIN\n[2] REGISTER\n[8] EXIT")
-
     opcao = int(input("\nChoose an option: "))
-    
+
     if opcao == 1:
         login = input("\nEnter your username: ")
         senha = input("Enter your password: ")
-        
-        if login in usuarios and usuarios[login]['senha'] == senha:
+
+        if login in usuarios and usuarios[login].senha == senha:
             print("\nLogin successful!\n")
-            
+            conta = usuarios[login]
+
             while True:
                 print("\n[3] DEPÓSITO\n[4] SAQUE\n[5] TRANSFERÊNCIA DE CONTA\n[6] CONSULTA DE EXTRATO\n[7] VOLTAR")
-                opcao = int(input("\nChoose an option: "))            
-                
-                print(usuarios)
-                
+                opcao = int(input("\nChoose an option: "))
+
                 if opcao == 3:
-                    deposito()
-                
+                    conta.deposito()
+
                 elif opcao == 4:
-                    saque()
-                
+                    conta.saque()
+
                 elif opcao == 5:
-                    transferencia()          
-                                    
+                    user_transfer = input("\nEnter the username to transfer: ")
+                    if user_transfer in usuarios:
+                        destinatario = usuarios[user_transfer]
+                        conta.transferencia(destinatario)
+                    else:
+                        print("\nUser not found!\n")
+
                 elif opcao == 6:
-                    consulta_extrato()
-                
+                    conta.consulta_extrato()
+
                 elif opcao == 7:
-                    print("\nReturning to the main menu...")
                     break
-                
         else:
             print("\nLogin or password failed!\n")
-            continue 
-         
+
     elif opcao == 2:
         login = input("\nEnter a username: ")
-    
-        if login in usuarios.keys():
+        if login in usuarios:
             print("\nThis user is already registered!\n")
-            continue
-            
         else:
             senha = input("Enter a password: ")
-            usuarios[login] = {'senha': senha, 'saldo': 0}
+            usuarios[login] = ContaBancaria(login, senha)
             print("\nUser registered successfully!\n")
-            
-            print(usuarios)
-    
+
     elif opcao == 8:
         print("\nExiting...")
         break
