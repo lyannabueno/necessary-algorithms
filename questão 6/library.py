bd2fff7feab210f1c8a0af8f689106a752b6c1a7
ANA FLÁVIA BUENO GOMES <06006399@acad.unifeso.lan>
2024-10-29 21:24:57 -0300

    sixth commit

--- questão 6/library.py
+++ questão 6/library.py
@@ -21,7 +21,7 @@ coluna_nome = ler_membros['nome']
 membros_livros_locados = {}
 
 while True:
-    opcao = int(input("\nOlá! Seja bem-vindo ao acervo online da instituição!\n\n[1] - PEGAR LIVROS\n[2] - DEVOLVER LIVROS\n[3] - SAIR\n\nO que você gostaria de fazer? "))
+    opcao = int(input("\nOlá! Seja bem-vindo ao acervo online da instituição!\n\n[1] - PEGAR LIVROS\n[2] - DEVOLVER LIVROS\n[3] - HISTÓRICO\n[4] - SAIR\n\nO que você gostaria de fazer? "))
 
     if opcao == 1:
         visualizar_livros = int(input("\n[1] - SIM\n[2] - NÃO\n\nDeseja visualizar os livros disponíveis para empréstimo? "))
@@ -79,25 +79,75 @@ while True:
 
                     if coluna_matricula.isin([membro_locacao]).any(): # verifica se a matrícula digitada está na lista de membros e retona True ou False
                         if membro_locacao in membros_livros_locados and livro_locacao_autor in [livro[0] for livro in membros_livros_locados[membro_locacao]]:
-                            print('\nO mesmo aluno não pode pegar o mesmo livro...\n')
+                            print('\nO mesmo aluno não pode pegar o mesmo livro...')
                         else:
                             if membro_locacao in coluna_matricula.values:
                                 ler_livros.loc[ler_livros['titulo'].str.lower() == livro_locacao_autor.lower(), 'quantidade'] -= 1
                                 membros_livros_locados.setdefault(membro_locacao, []).append((livro_locacao_autor, locacao.strftime("%d/%m/%Y"), devolucao.strftime("%d/%m/%Y"))) # adiciona um registro de empréstimo para o membro, com informações sobre o livro, data de locação e data de devolução
-                                print(f'\nLivro {livro_locacao_autor} emprestado com sucesso!\n\nVocê o pegou emprestado dia {locacao.strftime("%d/%m/%Y")} e terá que devolvê-lo dia {devolucao.strftime("%d/%m/%Y")}!\n\nAproveite a leitura!\n')           
+                                print(f'\nLivro {livro_locacao_autor} emprestado com sucesso!\n\nVocê o pegou emprestado dia {locacao.strftime("%d/%m/%Y")} e terá que devolvê-lo dia {devolucao.strftime("%d/%m/%Y")}!\n\nAproveite a leitura!')           
                     else:
-                        print('\nMembros não pertencentes à instituição não podem alocar livros...\n')
+                        print('\nMembros não pertencentes à instituição não podem alocar livros...')
                 else:
                     print('\nTodos nossos livros desse título foram esgotados...')
             else:
-                print('\nLivro não encontrado...\n')
+                print('\nLivro não encontrado...')
         else:
-            print("\nAutor ou livro não encontrado...\n")
+            print("\nAutor ou livro não encontrado...")
 
-    elif opcao == 2: # escrever o código da devolução
-        pass
+    elif opcao == 2:
+        livro_locacao = input("\nDigite o título ou autor do livro você está devolvendo: ")
+
+        # verificação que não é sensível a maiúsculas ou minúsculas
+        verificacao_autor = ler_livros['autores'].str.contains(livro_locacao, case=False)
+        verificacao_livro = ler_livros['titulo'].str.contains(livro_locacao, case=False)
+
+        if any(verificacao_livro):
+            quantidade_livro_atual = ler_livros.loc[verificacao_livro, 'quantidade'].values[0] # verifica a quantidade de livros específicos
+
+            if quantidade_livro_atual > 0:
+                membro_locacao = int(input("\nDigite sua matrícula: "))
+                
+                if membro_locacao in membros_livros_locados and livro_locacao in [livro[0] for livro in membros_livros_locados[membro_locacao]]:
+                    ler_livros.loc[verificacao_livro, 'quantidade'] += 1
+                    quantidade_livro_emprestado -= 1
+
+                    print(f'\nLivro {livro_locacao} devolvido com sucesso!\n\nObrigado por devolvê-lo!')
+                else:
+                    print('\nLivro não foi locado por você...')
+            else:
+                    print('\nLivro não encontrado...')
+
+        elif any(verificacao_autor):
+            livros_autor_especifico = ler_livros[verificacao_autor]
+            livro_locacao_autor = input('\nDiga o nome livro que pertencem a esse autor para fazer a devolução: ')
+
+            if livro_locacao_autor in livros_autor_especifico['titulo'].str.lower().values:
+                quantidade_livro_atual = livros_autor_especifico.loc[livros_autor_especifico['titulo'].str.lower() == livro_locacao_autor.lower(), 'quantidade'].values[0] 
+
+                if quantidade_livro_atual > 0:
+                    membro_locacao = int(input("\nDigite sua matrícula: "))
+                
+                    if membro_locacao in membros_livros_locados and livro_locacao in [livro[0] for livro in membros_livros_locados[membro_locacao]]:
+                        ler_livros.loc[ler_livros['titulo'].str.lower() == livro_locacao_autor, 'quantidade'] += 1
+                        quantidade_livro_emprestado -= 1
+
+                        membros_livros_locados[membro_locacao] = [livro for livro in membros_livros_locados[membro_locacao] if livro[0].lower() != livro_locacao_autor]
+
+                        print(f'\nLivro {livro_locacao} devolvido com sucesso!\n\nObrigado por devolvê-lo!')
+                    else:
+                        print('\nLivro não foi locado por você...')
+                else:
+                    print('\nLivro não encontrado...')
+            else:
+                print("\nAutor ou livro não encontrado...\n")
+
+    # existência de atraso (R$ 10, 00 por dia de atraso)
 
     elif opcao == 3:
+        pass
+        # visualização do histórico de dia de alocação, devolução, quantidade de atraso e total a ser pago
+
+    elif opcao == 4:
         print('\nAté mais!\n\nSaindo...\n')
         break
 
